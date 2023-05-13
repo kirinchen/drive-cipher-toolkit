34f0b5957cb8925ec49672a8032ae4e363ec898c
kirin <kirin.chen1001@gmail.com>
2023-05-14 01:23:00 +0800

    TODO impl new google api

--- src/GApiService.tsx
+++ src/GApiService.tsx
@@ -1,5 +1,10 @@
 import { JsLoaderService } from "./JsLoaderSservice";
 
+TODO https://developers.google.com/drive/api/quickstart/js?hl=zh-tw
+TODO https://developers.google.com/identity/oauth2/web/guides/migration-to-gis?hl=zh-tw#gis-popup-ux
+
+const DISCOVERY_DOC = 'https://www.googleapis.com/discovery/v1/apis/drive/v3/rest';
+
 export enum AuthState {
     API_INI_RUN = "API_INI_RUN",
     API_SCRIPT_LOADED = "API_SCRIPT_LOADED",
@@ -21,14 +26,21 @@ export class GApiService {
 
     public async init(): Promise<void> {
         console.log("init GAPI!!");
-        const ri = await JsLoaderService.loadScript({
-            id: 'gapi',
+        await JsLoaderService.loadScript({
+            id: 'gapiScript',
             src: 'https://apis.google.com/js/api.js'
         }, se => {
             se.async = true;
             se.defer = true;
         });
-        if (!ri.loaded) { throw new Error('not load gapi!'); }
+        await JsLoaderService.loadScript({
+            id: 'gsiScript',
+            src: 'https://accounts.google.com/gsi/client'
+        }, se => {
+            se.async = true;
+            se.defer = true;
+        });
+
         console.log(globalThis.gapi + " !!!!Loaded");
         this.authState = AuthState.API_SCRIPT_LOADED;
         await this.createClient();
@@ -40,38 +52,24 @@ export class GApiService {
 
     private async createClient(): Promise<void> {
         return new Promise((rev, rej) => {
-            globalThis.gapi.load('client:auth2', async () => { rev(); });
+            // globalThis.gapi.load('client:auth2', async () => { rev(); });
+            globalThis.gapi.load('client', { callback: rev, onerror: rej });
         });
+
     }
 
     private async initGApiClient(): Promise<void> {
-        try {
-            await globalThis.gapi.client.init({
-                apiKey: process.env.REACT_APP_GAPI_API_KEY,
-                clientId: process.env.REACT_APP_GAPI_OAUTH_CLIENT_ID,
-                discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/drive/v3/rest'],
-                plugin_name: 'google-drive-secret-toolkit',
-                scope: this.scopeStr(
-                    [
-                        'https://www.googleapis.com/auth/drive',
-                        'https://www.googleapis.com/auth/drive.file',
-                        'https://www.googleapis.com/auth/drive.readonly',
-                        'https://www.googleapis.com/auth/drive.metadata.readonly',
-                        'https://www.googleapis.com/auth/drive.appdata',
-                        'https://www.googleapis.com/auth/drive.metadata',
-                        'https://www.googleapis.com/auth/drive.photos.readonly'
-                    ]
-                )
+        await globalThis.gapi.client.init({
+            apiKey: process.env.REACT_APP_GAPI_API_KEY,
+            discoveryDocs: [DISCOVERY_DOC],
+        })
+        // .then(function () {  // Load the Calendar API discovery document.
+        //     // globalThis.gapi.client.load('https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest');
+        // });
 
-
-            });
-
-        } catch (ex) {
-            alert(JSON.stringify(ex, null, 2));
-        }
-        const auth = globalThis.gapi.auth2.getAuthInstance();
-        auth.isSignedIn.listen((s: boolean) => { this.updateSigninStatus(s); });
-        this.updateSigninStatus(globalThis.gapi.auth2.getAuthInstance().isSignedIn.get());
+        // const auth = globalThis.gapi.auth2.getAuthInstance();
+        // auth.isSignedIn.listen((s: boolean) => { this.updateSigninStatus(s); });
+        // this.updateSigninStatus(globalThis.gapi.auth2.getAuthInstance().isSignedIn.get());
     }
 
     private scopeStr(sc: string[]): string {
--- src/JsLoaderSservice.ts
+++ src/JsLoaderSservice.ts
@@ -15,30 +15,26 @@ export interface ScriptLoadInfo {
 export class JsLoaderService {
 
     private static scripts = new Array<Script>();
-    constructor() { }
 
     public static async loadScript(sc: Script, scEx: (e: HTMLScriptElement) => void): Promise<ScriptLoadInfo> {
         return new Promise((resolve, reject) => {
             // resolve if already loaded
-            const esc = this.scripts.find(s => s.id === sc.id);
-            if (esc) {
-                resolve({ script: esc.script as any, loaded: true, status: 'Already Loaded' });
-            }
-            else {
-                // load script
-                const script = document.createElement('script');
-                script.type = 'text/javascript';
-                script.id = sc.id;
-                script.src = sc.src;
-                sc.script = script;
-                scEx(script);
-                script.onload = () => {
-                    this.scripts.push(sc);
-                    resolve({ script, loaded: true, status: 'Loaded' });
-                };
-                script.onerror = (error: any) => resolve({ script: null, loaded: false, status: 'Loaded' });
-                document.getElementsByTagName('body')[0].appendChild(script);
-            }
+
+            // load script
+            const esc: HTMLScriptElement = document.getElementById(sc.id) as HTMLScriptElement;
+            const script = esc == null ? document.createElement('script') : esc;
+            script.type = 'text/javascript';
+            script.id = sc.id;
+            script.src = sc.src;
+            sc.script = script;
+            scEx(script);
+            script.onload = () => {
+                this.scripts.push(sc);
+                resolve({ script, loaded: true, status: 'Loaded' });
+            };
+            script.onerror = (error: any) => reject({ script: null, loaded: false, status: 'Loaded' });
+            document.getElementsByTagName('body')[0].appendChild(script);
+
         });
     }
 }
